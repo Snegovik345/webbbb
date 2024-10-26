08681af879cf31de4ca5778d9ad28da169db988e
Snegovik345 <romazemlanoy5051@gmail.com>
2024-10-26 19:39:10 +0700

    Create webbbbbb.py

---
+++ webbbbb/webbbbbb.py
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Игры которые выйдут в 2025 году:</h1>
+    <h2>На консолях и пк:</h2>
+    <h2>Список самых крупных ААА проектов </h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/WS0vTRl_2PQ?si=vh7qDyJ4Hvr9dwpI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <li>GTA 6</li>
+        <li>Mafia: The Old Country</li>
+        <li>Doom: The Dark Ages</li>
+        <li>Subnautica 2</li>
+        <li>Borderlands 4</li>
+        <li>Civilization VII</li>
+        <li>Heroes of Might and Magic: Olden Era</li>
+        <li>Ghost of Yotei</li>
+        <li>Little Nightmares 3</li>
+        <li>Death Stranding 2</li>
+        <li>Kingdom Come Deliverance 2<li>
+        
+    </ul>
+</body>
+</html>
